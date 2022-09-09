0083ccf3748bf84861da4b9be0d4ce49ccadf18f
safiyasharif1 <safiya.sharif@city.ac.uk>
2022-09-09 20:12:34 +0100

    fixed end link

--- game.js
+++ game.js
@@ -125,7 +125,7 @@ getNewQuestion = () =>{
 
     if(availableQuestions.length === 0 || questionCounter>= MAX_QUESTIONS){
         localStorage.setItem("mostRecentScore", score);
-        return window.location.assign("/end.html");
+        return window.location.assign("https://safiyasharif1.github.io/quiz/end.html");
 
     }
     questionCounter ++;
